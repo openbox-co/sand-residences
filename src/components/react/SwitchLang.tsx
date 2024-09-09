18b63a21cdeb4c5d62ed38c42db0c3d6b538363e
samuelrdg11 <samuelrdg11@gmail.com>
2024-09-08 19:47:48 -0500

    change:: enable functionality to switch languages

--- src/components/react/SwitchLang.tsx
+++ src/components/react/SwitchLang.tsx
@@ -1,4 +1,4 @@
-/* import { getLangFromUrl, useTranslatedPath } from "@/utils/i18n"; */
+import { getLangFromUrl, useTranslatedPath } from "@/utils/i18n";
 import { ChevronDownIcon } from '@heroicons/react/24/solid'
 import { useState } from "react";
 
@@ -7,20 +7,20 @@ interface Props {
 }
 
 export default function SwitchLang({ url }: Props) {
-  /*  const lang = getLangFromUrl(url);
-   const translatePath = useTranslatedPath(lang);
-   const [isChecked, setIsChecked] = useState(lang === 'es');
-   const handleChange = () => {
-     setIsChecked((prevCheck) => !prevCheck);
-     const nextLang = isChecked ? 'en' : 'es';
-     window.location.href = translatePath('/', nextLang);
-   }; */
+  const lang = getLangFromUrl(url);
+  const translatePath = useTranslatedPath(lang);
+  const [isChecked, setIsChecked] = useState(lang === 'es');
+  const handleChange = () => {
+    setIsChecked((prevCheck) => !prevCheck);
+    const nextLang = isChecked ? 'en' : 'es';
+    window.location.href = translatePath('/', nextLang);
+  };
 
   return (
     <section className="relative inline-block hover:bg-blackBroker hover:bg-opacity-10 rounded-lg">
       <select
-        /*   value={lang}
-          onChange={handleChange} */
+        value={lang}
+        onChange={handleChange}
         className="appearance-none w-full bg-transparent py-2 px-4 pr-8 rounded leading-tight focus:outline-none font-bold text-[#17161A]"
       >
         <option value='es'>ES</option>
