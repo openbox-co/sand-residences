8316f3858cc3035d915d43842247f62911510ae6
samuelrdg11 <samuelrdg11@gmail.com>
2024-09-08 19:45:52 -0500

    add:: i18n (translate) config

---
+++ src/i18n.ts
@@ -0,0 +1,22 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+export const showDefaultLang = false;
+
+export const i18n = {
+  es: {
+    welcome: "Bienvenido a",
+    welcome_subtitle:
+      "Poseidonia Residences en Cana Bay es un desarrollo residencial de lujo que combina la elegancia clásica con la comodidad moderna. Ubicado en el exclusivo complejo Cana Bay en Punta Cana, este proyecto ofrece un estilo de vida sofisticado y sereno. Inspirado en las villas históricas europeas, Poseidonia Residences brinda una experiencia de lujo mediterráneo en un entorno tropical caribeño, pensado para quienes buscan tranquilidad y confort en un espacio exclusivo.",
+    welcome_button: "Reserva tu cita hoy",
+  },
+  en: {
+    welcome: "Welcome to",
+    welcome_subtitle:
+      "Poseidonia Residences at Cana Bay is a luxury residential development that combines classic elegance with modern comfort. Located in the exclusive Cana Bay complex in Punta Cana, this project offers a sophisticated and serene lifestyle. Inspired by historic European villas, Poseidonia Residences provides a Mediterranean luxury experience in a tropical Caribbean setting, designed for those seeking tranquility and comfort in an exclusive space.",
+    welcome_button: "Book your appointment today",
+  },
+};
