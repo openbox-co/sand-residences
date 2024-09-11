08cb6b326d68eca92d226479ff6c1c4d96c415aa
samuelrdg11 <samuelrdg11@gmail.com>
2024-09-11 10:56:05 -0500

    change:: partial update translation file

--- src/i18n.ts
+++ src/i18n.ts
@@ -8,15 +8,94 @@ export const showDefaultLang = false;
 
 export const i18n = {
   es: {
+    //Form
+    title: "Tu Santuario de Lujo,",
+    //Welcome
     welcome: "Bienvenido a",
     welcome_subtitle:
-      "Poseidonia Residences en Cana Bay es un desarrollo residencial de lujo que combina la elegancia clásica con la comodidad moderna. Ubicado en el exclusivo complejo Cana Bay en Punta Cana, este proyecto ofrece un estilo de vida sofisticado y sereno. Inspirado en las villas históricas europeas, Poseidonia Residences brinda una experiencia de lujo mediterráneo en un entorno tropical caribeño, pensado para quienes buscan tranquilidad y confort en un espacio exclusivo.",
+      "Poseidonia Residences en Cana Bay es un proyecto residencial de lujo que combina a la perfección el estilo clásico con la comodidad contemporánea, brindando un entorno ideal para quienes buscan una vida sofisticada y tranquila. Inspirado en las villas históricas europeas, este enclave exclusivo en Punta Cana ofrece una experiencia única, fusionando el lujo mediterráneo con la serenidad tropical del Caribe. Diseñado para satisfacer los gustos más exigentes, Poseidonia Residences promete un espacio de tranquilidad y confort sin igual en el exclusivo complejo Cana Bay.",
     welcome_button: "Reserva tu cita hoy",
+    //Details
+    location: "Ubicación",
+    reserve: "Reserva con",
+    details_description:
+      "En Poseidonia Residences, los propietarios pueden elegir entre apartamentos completamente amueblados y decorados con elegancia. Cada vivienda maximiza el confort con amplias áreas de estar, cocinas modernas y balcones con vistas espectaculares. Los detalles y acabados de lujo destacan en cada aspecto del diseño.",
+    details_item1: "Piscina y Club de Playa Familiar",
+    details_item2: "Servicios de seguridad excepcional",
+    details_item3: "Gestión por la cadena internacional BY ASTON HOTEL",
+    //Apartments
+    apartments_title: "Conoce",
+    apartments_title1: "nuestros modelos",
+    apartments_title2: "de apartamentos",
+    apartments_button: "Brochure y Disponibilidad",
+    //Banner
+    banner_title: "Reserva con USD$1000",
+    banner_subtitle:
+      "eparación 10% (Incluye el monto de la reserva). Durante construcción 20% Contra entrega 70%",
+    //Tour3D
+    tour3d_button: "Tour 3D (Hacer recorrido)",
+    //Amenities
+    exclusives_black: "Amenidades",
+    exclusives_blue: "Exclusivas",
+    exclusives_description:
+      "Explora una amplia gama de amenidades diseñadas para enriquecer tu estilo de vida y ofrecerte momentos inolvidables.",
+    exclusives_description_bold:
+      "¡Descubre todas las maravillas que tenemos para ofrecer!",
+    slider1Title: "Club de Playa",
+    slider1:
+      "Relájate y disfruta del sol caribeño en nuestro exclusivo y lujoso Beach Club.",
+    slider2Title: "Canchas de Tenis",
+    slider2:
+      "Practica tu saque y revés en nuestras canchas de tenis de nivel profesional.",
+    slider3Title: "Campo de Golf",
+    slider3: "Mejora tu swing en nuestro campo de golf de primera categoría.",
+    slider4Title: "Canchas de Pádel",
+    slider4:
+      "Vive la adrenalina del pádel en nuestras canchas de primer nivel, para torneos y juegos casuales.",
   },
   en: {
+    //Form
+    title: "Your Luxury Sanctuary,",
+    //Welcome
     welcome: "Welcome to",
     welcome_subtitle:
-      "Poseidonia Residences at Cana Bay is a luxury residential development that combines classic elegance with modern comfort. Located in the exclusive Cana Bay complex in Punta Cana, this project offers a sophisticated and serene lifestyle. Inspired by historic European villas, Poseidonia Residences provides a Mediterranean luxury experience in a tropical Caribbean setting, designed for those seeking tranquility and comfort in an exclusive space.",
+      "Poseidonia Residences at Cana Bay is a luxury residential project that perfectly combines classic style with contemporary comfort, providing an ideal setting for those seeking a sophisticated and tranquil lifestyle. Inspired by historic European villas, this exclusive enclave in Punta Cana offers a unique experience, blending Mediterranean luxury with the tropical serenity of the Caribbean. Designed to meet the most discerning tastes, Poseidonia Residences promises an unparalleled space of tranquility and comfort within the exclusive Cana Bay complex.",
     welcome_button: "Book your appointment today",
+    //Details
+    location: "Location",
+    reserve: "Reserve with",
+    details_description:
+      "At Poseidonia Residences, owners can choose from fully furnished and elegantly decorated apartments. Each home maximizes comfort with spacious living areas, modern kitchens, and balconies offering spectacular views. Luxury details and finishes stand out in every aspect of the design",
+    details_item1: "Family Pool and Beach Club",
+    details_item2: "Exceptional security services",
+    details_item3: "Management by the international chain BY ASTON HOTEL",
+    //Apartments
+    apartments_title: "Meet",
+    apartments_title1: "our",
+    apartments_title2: "apartment models",
+    apartments_button: "Brochure and Availability",
+    //Banner
+    banner_title: "Reserve with USD$1000",
+    banner_subtitle:
+      "10% Down payment (Includes the reservation amount). During construction 20% Against delivery 70%",
+    //Tour3D
+    tour3d_button: "3D Tour (Take the tour)",
+    //Amenities
+    exclusives_black: "Amenities",
+    exclusives_blue: "Exclusives",
+    exclusives_description:
+      "Explore a wide range of amenities designed to enrich your lifestyle and offer you unforgettable moments.",
+    exclusives_description_bold: "Discover all the wonders we have to offer!",
+    slider1Title: "Beach Club",
+    slider1:
+      "Relax and enjoy the Caribbean sun in our exclusive and luxurious Beach Club.",
+    slider2Title: "Tennis Courts",
+    slider2:
+      "Practice your serve and backhand on our professional-level tennis courts.",
+    slider3Title: "Golf Course",
+    slider3: "Improve your swing on our first-class golf course.",
+    slider4Title: "Paddle Tennis Courts",
+    slider4:
+      "Experience the adrenaline of paddle tennis on our top-level courts, for tournaments and casual games.",
   },
 };
