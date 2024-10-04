4b87544b0137f21ba7a3c98464d2efcf98316bcc
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-10-03 23:17:17 -0500

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -13,48 +13,48 @@ export const i18n = {
     //Welcome
     welcome: "Bienvenido a",
     welcome_subtitle:
-      "Sand Residences  en Cana Bay es un proyecto residencial de lujo que combina a la perfección el estilo clásico con la comodidad contemporánea, brindando un entorno ideal para quienes buscan una vida sofisticada y tranquila. Inspirado en las villas históricas europeas, este enclave exclusivo en Punta Cana ofrece una experiencia única, fusionando el lujo mediterráneo con la serenidad tropical del Caribe. Diseñado para satisfacer los gustos más exigentes, Poseidonia Residences promete un espacio de tranquilidad y confort sin igual en el exclusivo complejo Cana Bay.",
+      "Sand Residences en Cap Cana es un desarrollo exclusivo que combina el diseño moderno con un ambiente caribeño de lujo, creando un entorno perfecto para quienes desean disfrutar de una vida relajada y elegante. Ubicado en el corazón de Ciudad Las Canas, este proyecto se inspira en la belleza natural y la sofisticación, ofreciendo apartamentos que maximizan el confort y la funcionalidad. Con una ubicación privilegiada cerca de las impresionantes playas de Cap Cana y sus atracciones de clase mundial, Sand Residences brinda una experiencia inigualable que mezcla el lujo contemporáneo con la serenidad tropical del Caribe.",
     welcome_button: "Reserva tu cita hoy",
     //Details
     location: "Ubicación",
     reserve: "Reserva con",
     details_description:
-      "En Poseidonia Residences, los propietarios pueden elegir entre apartamentos completamente amueblados y decorados con elegancia. Cada vivienda maximiza el confort con amplias áreas de estar, cocinas modernas y balcones con vistas espectaculares. Los detalles y acabados de lujo destacan en cada aspecto del diseño.",
-    details_item1: "Piscina y Club de Playa Familiar",
+      "En Sand Residences, los propietarios pueden disfrutar de apartamentos diseñados con atención al detalle y acabados de lujo. Cada unidad ofrece amplias áreas de estar, cocinas modernas y balcones privados con vistas impresionantes a las áreas comunes o a la naturaleza circundante. Los apartamentos están optimizados para maximizar el confort y la funcionalidad, con opciones que incluyen dos y tres habitaciones, decorados con un estilo elegante y contemporáneo. Las áreas están diseñadas para ofrecer la máxima comodidad, complementadas por la belleza tropical del entorno.",
+    details_item1: "Piscina para adultos y niños y gimnasio",
     details_item2: "Servicios de seguridad excepcional",
-    details_item3: "Gestión por la cadena internacional BY ASTON HOTEL",
+    details_item3: "Disfruta de la grandeza de un gran vestíbulo",
     //Apartments
     apartments_title: "Conoce",
     apartments_title1: "nuestros modelos",
     apartments_title2: "de apartamentos",
     apartments_button: "Brochure y Disponibilidad",
     //Banner
-    banner_title: "Reserva con USD$1000",
+    banner_title: "Reserva con USD$3000",
     banner_subtitle:
-      "eparación 10% (Incluye el monto de la reserva). Durante construcción 20% Contra entrega 70%",
+      "Firma de contrato 15%. Durante la construcción 35%",
     //Tour3D
     tour3d_button: "Tour 3D (Hacer recorrido)",
     //Amenities
     exclusives_black: "Amenidades",
     exclusives_blue: "Exclusivas",
     exclusives_description:
-      "Explora una amplia gama de amenidades diseñadas para enriquecer tu estilo de vida y ofrecerte momentos inolvidables.",
+      "Cada espacio en Sand Residences está pensado para maximizar tu confort. Disfruta también de zonas especiales para niños, un área de mascotas y amplias terrazas para relajarte al final del día.",
     exclusives_description_bold:
       "¡Descubre todas las maravillas que tenemos para ofrecer!",
-    slider1Title: "Club de Playa",
+    slider1Title: "Piscina Privada",
     slider1:
-      "Relájate y disfruta del sol caribeño en nuestro exclusivo y lujoso Beach Club.",
-    slider2Title: "Canchas de Tenis",
+      "Disfruta de un refrescante baño en nuestra exclusiva piscina, rodeada de un entorno tropical.",
+    slider2Title: "Gimnasio Moderno",
     slider2:
-      "Practica tu saque y revés en nuestras canchas de tenis de nivel profesional.",
-    slider3Title: "Campo de Golf",
-    slider3: "Mejora tu swing en nuestro campo de golf de primera categoría.",
-    slider4Title: "Canchas de Pádel",
+      "Mantente en forma en nuestro gimnasio completamente equipado, sin salir de casa.",
+    slider3Title: "Zona de BBQ y Gazebo",
+    slider3: "Comparte momentos especiales con familia y amigos en nuestra cómoda área de BBQ.",
+    slider4Title: "Amplios Balcones",
     slider4:
-      "Vive la adrenalina del pádel en nuestras canchas de primer nivel, para torneos y juegos casuales.",
+      "Disfruta de vistas panorámicas desde tu balcón privado, ideal para relajarte y admirar el entorno tropical que rodea tu hogar.",
     //PrivilegedLocation
     private_location_blue: "Ubicación Privilegiada",
-    private_location_black: "En Cana Bay",
+    private_location_black: "En Cap Cana",
     private_item1:
       "Hard Rock Hotel & Casino: Un destino popular para entretenimiento y ocio.",
     private_item2:
@@ -84,47 +84,46 @@ export const i18n = {
     //Welcome
     welcome: "Welcome to",
     welcome_subtitle:
-      "Sand Residences  at Cana Bay is a luxury residential project that perfectly combines classic style with contemporary comfort, providing an ideal setting for those seeking a sophisticated and tranquil lifestyle. Inspired by historic European villas, this exclusive enclave in Punta Cana offers a unique experience, blending Mediterranean luxury with the tropical serenity of the Caribbean. Designed to meet the most discerning tastes, Poseidonia Residences promises an unparalleled space of tranquility and comfort within the exclusive Cana Bay complex.",
+      "Sand Residences in Cap Cana is an exclusive development that combines modern design with a luxurious Caribbean atmosphere, creating a perfect environment for those who wish to enjoy a relaxed and elegant life. Located in the heart of Ciudad Las Canas, this project is inspired by natural beauty and sophistication, offering apartments that maximize comfort and functionality. With a privileged location near the stunning beaches of Cap Cana and its world-class attractions, Sand Residences provides an unparalleled experience that blends contemporary luxury with the tropical serenity of the Caribbean.",
     welcome_button: "Book your appointment today",
     //Details
     location: "Location",
     reserve: "Reserve with",
     details_description:
-      "At Poseidonia Residences, owners can choose from fully furnished and elegantly decorated apartments. Each home maximizes comfort with spacious living areas, modern kitchens, and balconies offering spectacular views. Luxury details and finishes stand out in every aspect of the design",
-    details_item1: "Family Pool and Beach Club",
+      "At Sand Residences, owners can enjoy apartments designed with attention to detail and luxury finishes. Each unit offers spacious living areas, modern kitchens, and private balconies with stunning views of the common areas or the surrounding nature. The apartments are optimized to maximize comfort and functionality, with options that include two and three bedrooms, decorated in an elegant and contemporary style. The areas are designed to offer maximum comfort, complemented by the tropical beauty of the surroundings.",
+    details_item1: "Pool for adults and children and gym",
     details_item2: "Exceptional security services",
-    details_item3: "Management by the international chain BY ASTON HOTEL",
+    details_item3: "Enjoy the grandeur of a grand lobby",
     //Apartments
     apartments_title: "Meet",
     apartments_title1: "our",
     apartments_title2: "apartment models",
     apartments_button: "Brochure and Availability",
     //Banner
-    banner_title: "Reserve with USD$1000",
-    banner_subtitle:
-      "10% Down payment (Includes the reservation amount). During construction 20% Against delivery 70%",
+    banner_title: "Reserve with USD$3000",
+    banner_subtitle: "Sign the contract 15%. During construction 35%",
     //Tour3D
     tour3d_button: "3D Tour (Take the tour)",
     //Amenities
     exclusives_black: "Amenities",
     exclusives_blue: "Exclusives",
     exclusives_description:
-      "Explore a wide range of amenities designed to enrich your lifestyle and offer you unforgettable moments.",
+      "Each space in Sand Residences is designed to maximize your comfort. Also enjoy special areas for children, a pet area, and spacious terraces to relax at the end of the day.",
     exclusives_description_bold: "Discover all the wonders we have to offer!",
-    slider1Title: "Beach Club",
+    slider1Title: "Private Pool",
     slider1:
-      "Relax and enjoy the Caribbean sun in our exclusive and luxurious Beach Club.",
-    slider2Title: "Tennis Courts",
+      "Enjoy a refreshing swim in our exclusive pool, surrounded by a tropical environment.",
+    slider2Title: "Modern Gym",
     slider2:
-      "Practice your serve and backhand on our professional-level tennis courts.",
-    slider3Title: "Golf Course",
-    slider3: "Improve your swing on our first-class golf course.",
-    slider4Title: "Paddle Tennis Courts",
+      "Stay in shape in our fully equipped gym, without leaving home.",
+    slider3Title: "BBQ Area and Gazebo",
+    slider3: "Share special moments with family and friends in our comfortable BBQ area.",
+    slider4Title: "Spacious Balconies",
     slider4:
-      "Experience the adrenaline of paddle tennis on our top-level courts, for tournaments and casual games.",
+      "Enjoy panoramic views from your private balcony, ideal for relaxing and admiring the tropical surroundings of your home.",
     //PrivilegedLocation
     private_location_blue: "Privileged Location",
-    private_location_black: "In Cana Bay",
+    private_location_black: "In Cap Cana",
     private_item1:
       "Hard Rock Hotel & Casino: A popular destination for entertainment and leisure.",
     private_item2:
